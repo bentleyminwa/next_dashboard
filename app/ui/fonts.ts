4bcdcd8967c11b2a1051e1c76c96de1d14883931
Bentley Minwa <bentleyminwa@gmail.com>
2025-02-24 19:02:45 +0300

    Added dashboard layout and pages for customers, invoices, and main dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana, Quicksand } from "next/font/google";
+
+export const quicksand = Quicksand({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
